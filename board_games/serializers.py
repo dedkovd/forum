f1fcef879ee6bcc38709c98b45e2cdc7c62e7efd
Denis V. Dedkov <denis.v.dedkov@gmail.com>
2016-08-08 22:43:25 +0500

    Реализован список городов для страны

--- board_games/serializers.py
+++ board_games/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
-from board_games.models import Country
+from board_games.models import Country, City
 
 class CountrySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Country
-        fields = ('name', 'internal_id')
+        fields = ('name', 'id')
--- board_games/views.py
+++ board_games/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from board_games.models import Country
-from board_games.serializers import CountrySerializer
+from rest_framework.decorators import detail_route
+from rest_framework.response import Response
+from board_games.models import *
+from board_games.serializers import *
 
 class CountryViewSet(viewsets.ModelViewSet):
-    queryset = Country.objects.all()
-    serializer_class = CountrySerializer
+	queryset = Country.objects.all()
+	serializer_class = CountrySerializer
 
+	@detail_route()
+    	def cities(self, request, pk):
+		cities = City.objects.filter(country = self.get_object())
+		
+		serializer = self.get_serializer(cities, many=True)
+		return Response(serializer.data)
