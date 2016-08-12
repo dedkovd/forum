040285867edf213f933cd2e8ee8c4b628f10d9f5
Denis V. Dedkov <denis.v.dedkov@gmail.com>
2016-08-12 08:55:53 +0500

    Добавил документацию для сериализаторов

--- board_games/serializers.py
+++ board_games/serializers.py
@@ -1,18 +1,34 @@
+# -*- coding: utf-8 -*-
+'''
+Модуль с описанием сериализаторов для объектов
+
+Данный модуль зависит от пакета django-restframework
+'''
+
 from rest_framework import serializers
 from rest_framework_recursive.fields import RecursiveField
 from board_games.models import Country, Category, Post, CustomUser, Image
 
 class CountrySerializer(serializers.HyperlinkedModelSerializer):
+    '''
+    Сериализатор для стран
+    '''
     class Meta:
         model = Country
         fields = ('name', 'id',)
 
 class CategorySerializer(serializers.HyperlinkedModelSerializer):
+    '''
+    Сериализатор для категорий
+    '''
     class Meta:
         model = Category
         fields = ('category_name','id',)
 
 class PostsSerializer(serializers.ModelSerializer):
+    '''
+    Сериализатор для постов
+    '''
     replies = serializers.ListField(child = RecursiveField(), read_only = True)
     class Meta:
         model = Post
@@ -29,11 +45,17 @@ class PostsSerializer(serializers.ModelSerializer):
                   )
 
 class ImageSerializer(serializers.ModelSerializer):
+    '''
+    Сериализатор для изображений
+    '''
     class Meta:
         model = Image
         fields = ('image_file', 'parent_post')
 
 class CustomUserSerializer(serializers.ModelSerializer):
+    '''
+    Сериализатор для кастомного пользователя
+    '''
     starred_posts = PostsSerializer(many=True, read_only=True)
     class Meta:
         model = CustomUser
