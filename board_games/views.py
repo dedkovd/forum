1cba4d1ed7a4c84a6fac94616b3af8c04ffeeef8
Denis V. Dedkov <denis.v.dedkov@gmail.com>
2016-08-12 08:51:58 +0500

    Добавил документацию для вьюх

--- board_games/views.py
+++ board_games/views.py
@@ -1,4 +1,10 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
+'''
+Модуль с описанием представлений
+
+Данный модуль практически полностью зависит от django-restframework
+(для реализации REST)
+'''
 from django.db.models import Q
 from rest_framework import viewsets, permissions
 from rest_framework.decorators import * 
@@ -10,23 +16,36 @@ from board_games.models import *
 from board_games.serializers import *
 
 class CustomUserViewSet(viewsets.ReadOnlyModelViewSet):
+	'''
+	Представление перечня пользователей
+	'''
 	authentication_classes = (SessionAuthentication, TokenAuthentication,)
 	permission_classes = (IsAuthenticated,)
 	queryset = CustomUser.objects.all()
 	serializer_class = CustomUserSerializer
 
 class CountryViewSet(viewsets.ReadOnlyModelViewSet):
+	'''
+	Представление перечня стран
+	'''
 	queryset = Country.objects.all()
 	serializer_class = CountrySerializer
 
 	@detail_route()
     	def cities(self, request, pk):
+		'''
+		Детализация страны до городов
+		'''
 		cities = City.objects.filter(country = self.get_object())
 		
 		serializer = self.get_serializer(cities, many=True)
 		return Response(serializer.data)
 
 class IsAdminOrReadOnly(permissions.BasePermission):
+	'''
+	Дополнительный класс проверки прав - читать могут все,
+	редактировать тольо админ
+	'''
 	def has_permission(self, request, view):
 		if request.method in permissions.SAFE_METHODS:
 			return True
@@ -34,6 +53,10 @@ class IsAdminOrReadOnly(permissions.BasePermission):
 		return request.user.is_staff
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
+	'''
+	Дополнительный класс для проверки прав - читать могут все,
+	редактировать только владелец объекта
+	'''
 	def has_object_permission(self, request, view, obj):
 		if request.method in permissions.SAFE_METHODS:
 			return True
@@ -41,11 +64,19 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
 		return obj.owner.id == request.user.id
 
 class PostsViewSet(viewsets.ModelViewSet):
+	'''
+	Представление постов
+	'''
 	authentication_classes = (SessionAuthentication, TokenAuthentication,)
 	permission_classes = (IsOwnerOrReadOnly,)
 	serializer_class = PostsSerializer
 
 	def get_queryset(self):
+		'''
+		Запрос отображает в зависимости от статуса пользователя
+		или все посты или только посты пользователя + заапрувленные
+		админом
+		'''
 		is_staff = self.request.user.is_staff
 		if is_staff:
 			return Post.objects.all()
@@ -54,6 +85,10 @@ class PostsViewSet(viewsets.ModelViewSet):
 						   Q(owner = self.request.user))
 
 	def update(self, request, pk):
+		'''
+		Проверка что пользователь является владельцем поста или 
+		админом при редактировании
+		'''
 		post = Post.objects.get(pk = pk)
 		if not IsOwnerOrReadOnly().has_object_permission(request,self, post) and not request.user.is_staff:
 			return Response(status = 401)
@@ -67,6 +102,10 @@ class PostsViewSet(viewsets.ModelViewSet):
 
 	@detail_route(methods=['post','delete'])
 	def starred(self, request, pk):
+		'''
+		Детализация к постам, добавленным в избранное
+		(для добавления или удаления поста)
+		'''
 		if request.method == 'POST':
 			request.user.customuser.starred_posts.add(pk)
 
@@ -78,6 +117,9 @@ class PostsViewSet(viewsets.ModelViewSet):
 	@detail_route(methods=['get', 'post',],)
 	@parser_classes((FormParser, MultiPartParser,))
 	def images(self, request, pk):
+		'''
+		Детализация к изображениям поста
+		'''
 		post = Post.objects.get(pk = pk)
 		if not IsOwnerOrReadOnly().has_object_permission(request,self, post):
 			return Response(status = 401)
@@ -96,6 +138,9 @@ class PostsViewSet(viewsets.ModelViewSet):
 			return Response(serializer.data)
 
 class CategoriesViewSet(viewsets.ModelViewSet):
+	'''
+	Представление категорий
+	'''
 	authentication_classes = (SessionAuthentication, TokenAuthentication,)
 	permission_classes = (IsAuthenticated,IsAdminOrReadOnly,)
 	queryset = Category.objects.all()
@@ -103,6 +148,9 @@ class CategoriesViewSet(viewsets.ModelViewSet):
 
 	@detail_route(methods=['get', 'post'], permission_classes=[IsAuthenticated,])
 	def posts(self, request, pk):
+		'''
+		Детализация к постам категории
+		'''
 		if request.method == 'POST':
 			data = JSONParser().parse(request)
 			data['category'] = pk
@@ -128,6 +176,9 @@ class CategoriesViewSet(viewsets.ModelViewSet):
 @api_view(['GET'])
 @authentication_classes([TokenAuthentication,])
 def starred_posts(request):
+	'''
+	Перечень избранных постов для текущего пользователя
+	'''
 	posts = request.user.customuser.starred_posts
 	print posts
 
@@ -138,6 +189,9 @@ def starred_posts(request):
 @authentication_classes([TokenAuthentication,])
 @permission_classes([IsAdminUser,])
 def ban_user(request,pk):
+	'''
+	Забанить пользователя
+	'''
 	u = CustomUser.objects.get(pk = pk)
 	u.is_active = False
 	u.save()
@@ -147,6 +201,9 @@ def ban_user(request,pk):
 @authentication_classes([TokenAuthentication,])
 @permission_classes([IsAdminUser,])
 def unban_user(request,pk):
+	'''
+	Снять бан с пользователя
+	'''
 	u = CustomUser.objects.get(pk = pk)
 	u.is_active = True
 	u.save()
@@ -154,6 +211,9 @@ def unban_user(request,pk):
 
 @api_view(['POST'])
 def create_user(request):
+	'''
+	Зарегистрировать пользователя
+	'''
 	serializer = CustomUserSerializer(data = request.data)
 	if serializer.is_valid():
 		u = CustomUser.objects.create_user(
